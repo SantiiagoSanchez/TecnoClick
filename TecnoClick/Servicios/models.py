3d31bfcf8d1209b5b241314839135746968085fb
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-06-22 21:19:34 -0300

    Nueva aplicacion 'Servicio'

---
+++ TecnoClick/Servicios/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Servicio(models.Model):
+    Titulo=models.CharField(max_length=50)
+    Contenido=models.CharField(max_length=50)
+    Imagen=models.ImageField()
+    Created=models.DateTimeField(auto_now_add=True)
+    Updated=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Servicio"
+        verbose_name_plural = "Servicios"
+    
+    def __str__(self):
+        return self.Titulo
