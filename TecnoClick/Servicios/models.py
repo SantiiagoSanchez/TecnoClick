f1c623d45909ef3ec47fdfa4a66f4998c2b00b10
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-06-27 18:43:06 -0300

    Pagina de servicios mostrando todos los servicios ya con una buena estructura

--- TecnoClick/Servicios/models.py
+++ TecnoClick/Servicios/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Servicio(models.Model):
     Titulo=models.CharField(max_length=50)
-    Contenido=models.CharField(max_length=50)
+    Contenido=models.CharField(max_length=100)
     Imagen=models.ImageField(upload_to="img_servicios")
     Created=models.DateTimeField(auto_now_add=True)
     Updated=models.DateTimeField(auto_now_add=True)
