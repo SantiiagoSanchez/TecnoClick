962560a8b66d50261014ef5beecdb6c11720c568
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-06-30 12:34:40 -0300

    Pagina blog terminada(creo) y filtrar posteo por categoria

--- TecnoClick/Blog/views.py
+++ TecnoClick/Blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from .models import Posteo
+from .models import Posteo,Categoria
 # Create your views here.
 
 def blog (request):
     post = Posteo.objects.all()
     return render(request, "Blog/about.html", {"posteos": post})
+
+def categoria (request, categoria_id):
+
+    category = Categoria.objects.get(id = categoria_id)
+    post = Posteo.objects.filter(Categorias = category)
+    return render(request, "Blog/categoria.html", {'Categoria': category, 'posteos': post})
