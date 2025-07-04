e29cb3816ea62027a4fbe639b56f58d485776899
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-07-04 10:14:31 -0300

    Seguimos con el formulario de contacto

--- TecnoClick/Contacto/views.py
+++ TecnoClick/Contacto/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import FormContacto
 # Create your views here.
 
 def contacto (request):
 
     form = FormContacto()
+
+    if request.method == "POST":
+        form = FormContacto(data=request.POST)
+        if form.is_valid():
+            nombre = request.POST.get("Nombre")
+            email = request.POST.get("Email")
+            contenido = request.POST.get("Mensaje")
+        
+            return redirect("/contact/?valido")
+
+
 
     return render(request, "Contacto/contact.html", {"miFormulario": form})
