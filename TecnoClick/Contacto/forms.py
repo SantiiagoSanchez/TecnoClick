31bf63c189b799e4d1580630c984992a0496c44c
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-07-03 22:19:19 -0300

    Seguimos con el formulario de contacto, le puse un poco de css

--- TecnoClick/Contacto/forms.py
+++ TecnoClick/Contacto/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class FormContacto(forms.Form):
     Nombre = forms.CharField(label="Nombre", max_length=100, required=True)
     Email = forms.EmailField(label="Email", required=True)
-    Contenido = forms.CharField(label="Mensaje", max_length=200, required=True)
+    Contenido = forms.CharField(label="Mensaje", widget=forms.Textarea, max_length=200, required=True)
