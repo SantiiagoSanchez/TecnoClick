88a389d0e06c494c8ea3805358f8d9bc11aa7f52
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-07-12 00:29:32 -0300

    Views del carrito

--- TecnoClick/Carrito/views.py
+++ TecnoClick/Carrito/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .Carrito import Carrito
+from Tienda.models import Producto
 # Create your views here.
+
+def post_producto(request, idProd):
+
+    carro = Carrito(request)
+
+    producto = Producto.objects.get(id=idProd)
+
+    carro.addProducto(producto)
+
+    return redirect("store")
+
+def delete_producto(request, idProd):
+
+    carro = Carrito(request)
+
+    producto = Producto.objects.get(id=idProd)
+
+    carro.delete_producto(producto)
+
+    return redirect("store")
+
+def restar_producto(request, idProd):
+
+    carro = Carrito(request)
+
+    producto = Producto.objects.get(id=idProd)
+
+    carro.restarProducto(producto)
+
+    return redirect("store")
+
+def limpiar_carro(request):
+
+    carro = Carrito(request)
+
+    carro.limpiarCarrito()
+
+    return redirect("store")
