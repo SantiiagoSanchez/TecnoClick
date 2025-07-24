f3dc9ce50db4c2b967ec4862fbd5cd80f811d4db
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-07-24 11:27:08 -0300

    Login funcionando

--- TecnoClick/Autenticacion/urls.py
+++ TecnoClick/Autenticacion/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import VRegistro, cerrarsesion
+from .views import VRegistro, cerrarsesion, logear
 
 urlpatterns = [
     path('', VRegistro.as_view(), name="Autenticacion"),
-        path('cerrar_sesion', cerrarsesion, name="cerrarsesion"),
+    path('cerrar_sesion', cerrarsesion, name="cerrarsesion"),
+    path('logear', logear, name="logear"),
+
 
 ]
 
--- TecnoClick/Autenticacion/views.py
+++ TecnoClick/Autenticacion/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 # Create your views here.
 
@@ -30,6 +30,27 @@ class VRegistro (View):
 def cerrarsesion(request):
     logout(request)
     return redirect("Inicio")
+
+def logear(request):
+
+    form= AuthenticationForm()
+
+    if request.method=="POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre_user = form.cleaned_data.get("username")
+            contrasena = form.cleaned_data.get("password")
+            usuario = authenticate(username=nombre_user,password=contrasena)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect("Inicio")
+            else:
+                messages.error(request, "ERROR: Usuario no encontrado")
+        else:
+            messages.error(request, "Informacion incorrecta")
+
+    return render(request, "registro/login.html", {'Form': form})
+
 
 
 
