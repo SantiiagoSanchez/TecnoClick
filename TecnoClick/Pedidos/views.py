4883dc4981c2ecf3f453ec1f2ae0b35b90f38941
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-07-31 12:58:48 -0300

    Vistas de pedidos

--- TecnoClick/Pedidos/views.py
+++ TecnoClick/Pedidos/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from Carrito.Carrito import Carrito
+from TecnoClick.Pedidos.models import Pedido, LineaPedido
+from django.contrib import messages
 
 # Create your views here.
+
+@login_required(login_url="/Autenticacion/loguear")
+def procesar_pedido(request):
+    pedido=Pedido.object.create(user=request.user)
+    carrito=Carrito(request)
+    lineas_pedido = list()
+    for key, value in carrito.carrito.items():
+        lineas_pedido.append(LineaPedido(
+
+            producto_id=key,
+            cantidad=value["Cantidad"],
+            user=request.user,
+            pedido=pedido
+
+        ))
+
+    LineaPedido.objects.bulk_create(lineas_pedido) #Esto hace muchos insert into en la tabla linea de pedidos
+
+    enviar_mail(
+        pedido=pedido,
+        lineas_pedido=lineas_pedido,
+        nombre_user=request.username,
+        email_user=request.usermail  
+    )
+
+    messages.success(request, "El pedido fue creado correctamente")
+
+    return redirect("../store")
+
